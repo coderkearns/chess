81c70974029753adade8bee6fcb322fbf1e6819a
coderkearns <coder.kearns@gmail.com>
2026-09-10 10:30:10 -0600

    chore: generate toString helpers

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -108,4 +108,12 @@ public class ChessPiece {
     public int hashCode() {
         return Objects.hash(teamColor, pieceType);
     }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                teamColor +
+                " " + pieceType +
+                '}';
+    }
 }
