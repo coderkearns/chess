4cfed84557ccae9abbbabcc56750042ed4abe97b
Kennedy Web Developer <kennedyweb@byu.edu>
2026-09-14 11:19:36 -0600

    fix: pass in move set when computing horizontal and diagonal moves

--- shared/src/main/java/chess/movestrategies/DiagonalMoveStrategy.java
+++ shared/src/main/java/chess/movestrategies/DiagonalMoveStrategy.java
@@ -4,46 +4,51 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
 import java.util.Set;
 
 public class DiagonalMoveStrategy extends MoveStrategy {
-  public DiagonalMoveStrategy(ChessBoard board, ChessPosition myPosition,
-                              ChessPiece myPiece) {
-    super(board, myPosition, myPiece);
-  }
-
-  @Override
-  public void addMoves(Set<ChessMove> possibleMoves) {
-    // For each direction, keep moving forward until an invalid spot is found.
-    int row = myPosition.getRow();
-    int col = myPosition.getColumn();
-
-    // Up-right
-    for (int i = 0; i < 9; i++) {
-      ChessPosition newPosition = new ChessPosition(row + i, col + i);
-      if (!addPositionIfValid(newPosition))
-        break;
+    public DiagonalMoveStrategy(ChessBoard board, ChessPosition myPosition,
+                                ChessPiece myPiece) {
+        super(board, myPosition, myPiece);
     }
 
-    // Up-left
-    for (int i = 0; i < 9; i++) {
-      ChessPosition newPosition = new ChessPosition(row + i, col - i);
-      if (!addPositionIfValid(newPosition))
-        break;
-    }
+    @Override
+    public void addMoves(Set<ChessMove> possibleMoves) {
+        // For each direction, keep moving forward until an invalid spot is found.
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
 
-    // Down-right
-    for (int i = 0; i < 9; i++) {
-      ChessPosition newPosition = new ChessPosition(row - i, col + i);
-      if (!addPositionIfValid(newPosition))
-        break;
-    }
+        // Up-right
+        for (int i = 0; i < 9; i++) {
+            ChessPosition newPosition = new ChessPosition(row + i, col + i);
+          if (!addPositionIfValid(newPosition, possibleMoves)) {
+            break;
+          }
+        }
 
-    // Down-left
-    for (int i = 0; i < 9; i++) {
-      ChessPosition newPosition = new ChessPosition(row - i, col - i);
-      if (!addPositionIfValid(newPosition))
-        break;
+        // Up-left
+        for (int i = 0; i < 9; i++) {
+            ChessPosition newPosition = new ChessPosition(row + i, col - i);
+          if (!addPositionIfValid(newPosition, possibleMoves)) {
+            break;
+          }
+        }
+
+        // Down-right
+        for (int i = 0; i < 9; i++) {
+            ChessPosition newPosition = new ChessPosition(row - i, col + i);
+          if (!addPositionIfValid(newPosition, possibleMoves)) {
+            break;
+          }
+        }
+
+        // Down-left
+        for (int i = 0; i < 9; i++) {
+            ChessPosition newPosition = new ChessPosition(row - i, col - i);
+          if (!addPositionIfValid(newPosition, possibleMoves)) {
+            break;
+          }
+        }
     }
-  }
 }
--- shared/src/main/java/chess/movestrategies/HorizontalMoveStrategy.java
+++ shared/src/main/java/chess/movestrategies/HorizontalMoveStrategy.java
@@ -1,11 +1,11 @@
 package chess.movestrategies;
 
-import java.util.Set;
-
 import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
+import java.util.Set;
 
 public class HorizontalMoveStrategy extends MoveStrategy {
     public HorizontalMoveStrategy(ChessBoard board, ChessPosition myPosition, ChessPiece myPiece) {
@@ -21,29 +21,33 @@ public class HorizontalMoveStrategy extends MoveStrategy {
         // Vertical up
         for (int i = col + 1; i < 9; i++) {
             ChessPosition newPosition = new ChessPosition(row, i);
-            if (!addPositionIfValid(newPosition))
-              break;
+            if (!addPositionIfValid(newPosition, possibleMoves)) {
+                break;
+            }
         }
 
         // Vertical down
         for (int i = col - 1; i > 0; i--) {
             ChessPosition newPosition = new ChessPosition(row, i);
-            if (!addPositionIfValid(newPosition))
-              break;
+            if (!addPositionIfValid(newPosition, possibleMoves)) {
+                break;
+            }
         }
 
         // Horizontal right
         for (int i = row + 1; i < 9; i++) {
             ChessPosition newPosition = new ChessPosition(i, col);
-            if (!addPositionIfValid(newPosition))
-              break;
+            if (!addPositionIfValid(newPosition, possibleMoves)) {
+                break;
+            }
         }
 
         // Horizontal left
         for (int i = row - 1; i > 0; i--) {
             ChessPosition newPosition = new ChessPosition(i, col);
-            if (!addPositionIfValid(newPosition))
-              break;
+            if (!addPositionIfValid(newPosition, possibleMoves)) {
+                break;
+            }
         }
     }
 }
