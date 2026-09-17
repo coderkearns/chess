c1c81610bec00fd5c7bae825d8697a350ee86be2
Kennedy Web Developer <kennedyweb@byu.edu>
2026-09-17 15:27:11 -0600

    feat: store board in game

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -11,8 +11,10 @@ import java.util.Collection;
 public class ChessGame {
 
     private TeamColor teamTurn = TeamColor.WHITE;
+    private ChessBoard board = new ChessBoard();
 
     public ChessGame() {
+        board.resetBoard();
     }
 
     /**
